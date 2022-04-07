7eeecf3cb03b4798bd1f7b125cb0d10d936ff49e
Imran Hossen <freelancerimran81@gmail.com>
2022-04-07 10:08:54 +0600

    bootstrap install

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { getAuth } from 'firebase/auth'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import app from './firebase.init';
 
@@ -13,13 +14,17 @@ function App() {
   const handlePasswordBlur = event =>{
     console.log(event.target.value);
   }
+  const handleFormSubmit = event =>{
+    console.log('form submitted');
+    event.preventDefault();
+  }
 
   return (
     <div className="App">
-      <form >
+      <form onSubmit={handleFormSubmit}>
         <br /><br />
-        <input onBlur={handleEmailBlur} type="email" name="" id="" /><br /><br />
-        <input onBlur={handlePasswordBlur} type="password" name="" id="" /><br /><br />
+        <input onBlur={handleEmailBlur} type="email" name="" id="email" /><br /><br />
+        <input onBlur={handlePasswordBlur} type="password" name="" id="password" autoComplete='on'  /><br /><br />
         <input type="submit" value="LOGIN" />
       </form>
     </div>
